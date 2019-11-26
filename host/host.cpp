de46e1ce998c16de55afd34208231556b8ba9a7e
Ubuntu <hou@Oracle.sc3temum012u3livpuyxqmhubf.bx.internal.cloudapp.net>
2019-11-26 18:53:51 +0000

    network function

--- host/host.cpp
+++ host/host.cpp
@@ -10,6 +10,7 @@
 #include <netinet/in.h>
 
 #include <thread>
+#include <mutex>
 #include <iostream>
 #include <signal.h>
 #include <unistd.h>
@@ -18,6 +19,8 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
+
+#define REVOLVER(a, b) (a < 0) ? b - 1 : ((a == b) ? 0 : a)
 
 struct report{
     int index;
@@ -28,15 +31,24 @@ struct report{
     size_t report_data_size;
 };
 
-struct data_msg{
+/* message type:
+ * 0x01 data delivery
+ * 0x02 data request
+ * 0x04 leader election
+ * 0x08 reelection */
+
+struct message{
     int index;
+    char type;
     unsigned int msg_len;
+    double data;
     // Additional attributes
 };
 
-int sock, my_index, oracle_num;
+int *sock, my_index, oracle_num;
 struct sockaddr_in my_addr;
 struct sockaddr_in* peer_list;
+std::mutex mtx_lock;
 
 oe_enclave_t* create_enclave(const char* enclave_path)
 {
@@ -101,7 +113,8 @@ int init_peer_config(){
         }
     }
     
-    peer_list = (struct sockaddr_in*)malloc(sizeof(sockaddr_in) * oracle_num);
+    peer_list = (struct sockaddr_in *)malloc(sizeof(sockaddr_in) * oracle_num);
+    sock = (int *)malloc(sizeof(int) * oracle_num);
 
     for(int i = 0; i < oracle_num; i++){
 
@@ -124,59 +137,121 @@ int init_peer_config(){
 
         printf("Host: %d-th oracle ip addr is %s\n", i, buf);
     }
-    
+
     return 1;
 }
 
-void recv_report(oe_enclave_t* enclave){
-    struct sockaddr_in sender_addr;
-    struct report pkt;
+void accept_connection(int data){
+    int new_socket, cnt;
+    struct sockaddr_in new_addr;
+    socklen_t addr_size = sizeof(new_addr);
+    printf("Host: waiting for accept()... \n");
+    while(1){
+        new_socket = accept(sock[my_index], (struct sockaddr*)&new_addr, &addr_size);
+
+        if (new_socket < 0)
+            fprintf(stderr, "accept fail.\n");
+
+        /* Warning:
+         * Only odd 'oracle_num' is allowed. */
+        for(int i = my_index + 1, cnt = 0; cnt < oracle_num / 2; i++, cnt++){
+            i = REVOLVER(i, oracle_num);
+            if (strcmp(inet_ntoa(new_addr.sin_addr), inet_ntoa(peer_list[i].sin_addr)) == 0)
+                sock[i] = new_socket;
+        }
+
+        if (cnt > oracle_num /2)
+            fprintf(stderr, "Unauthorized node connection.\n"); 
+    }
+}
+
+int connect_to_peer(){
+    std::thread worker;
+
+    // Socket generation
+    sock[my_index] = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock[my_index] < 0){
+        fprintf(stderr, "socket generation fail.\n");
+        return -1;
+    }
+    
+    // Binding
+    if (bind(sock[my_index], (struct sockaddr *)&peer_list[my_index], sizeof(peer_list[my_index])) < 0){
+        fprintf(stderr, "bind fail.\n");
+        return -1;
+    }
+
+    // Accept
+    worker = std::thread(accept_connection, my_index);
+
+    // Connect
+
+    /* Warning:
+     * Only odd 'oracle_num' is allowed. */
+    for (int i = my_index - 1, cnt = 0; cnt < oracle_num / 2; i--, cnt++){
+        i = REVOLVER(i, oracle_num);
+        if (connect(sock[i], (struct sockaddr *)&peer_list[i], sizeof(peer_list[i])) < 0){
+            fprintf(stderr, "connection to %d-th node fail.\n", i);
+            return -1;
+        }
+        printf("Host: connection with %s succeed.\n", inet_ntoa(peer_list[i].sin_addr));
+    }
+    
+    if (worker.joinable() == true)
+        worker.join();
+
+    return 1;
+}
+
+void recv_report(oe_enclave_t* enclave, int index){
+    struct report *rcvd_report;
     uint8_t* pem_key = NULL;
     size_t pem_key_size = 0;
     uint8_t* remote_report = NULL;
     size_t remote_report_size = 0;
     oe_result_t result = OE_OK;
-    int cnt = 1, ret;
-    socklen_t addrlen = 10;
-    
-    printf("Host: recv_report...\n");
-    while(cnt != oracle_num){
-        // ECALL: verify_report_and_set_pubkey
-        recvfrom(sock, &pkt, sizeof(struct report), 0, (struct sockaddr*)&sender_addr, &addrlen);
-        printf("!TEST! remote report size = %lu\n", sizeof(pkt.report_data));
+    int ret, flag = 1;
 
-        pem_key = pkt.public_key;
-        pem_key_size = pkt.public_key_size;
-        remote_report = pkt.report_data;
-        remote_report_size = pkt.report_data_size;
+    while(flag){
+        if (recv(sock[index], (char *)rcvd_report, sizeof(struct report), 0) > 0){
+            printf("Listener-%d: report received.\n", index);
+            
+            memcpy(pem_key, rcvd_report->public_key, rcvd_report->public_key_size);
+            pem_key_size = rcvd_report->public_key_size;
+            memcpy(remote_report, rcvd_report->report_data, rcvd_report->report_data_size);
+            remote_report_size = rcvd_report->report_data_size;
+            
+            mtx_lock.lock();
+            result =  verify_report_and_set_pubkey(
+                enclave,
+                &ret,
+                pem_key,
+                pem_key_size,
+                remote_report,
+                remote_report_size);
 
-        printf("Host: verifying %d-th oracle nodes remote report and public key...\n", pkt.index);
-        result = verify_report_and_set_pubkey(
-            enclave,
-            &ret,
-            pem_key,
-            pem_key_size,
-            remote_report,
-            remote_report_size);
+            if ((result != OE_OK) || (ret != 0)){
+                printf(
+                    "Host: verify_report_and_set_pubkey failed. %s",
+                    oe_result_str(result));
+            }
+            else{
+                printf(
+                    "Host: %d-th oracle's remote attestation succeeded.\n",
+                    index);
+                flag = 0;
+            }
+            mtx_lock.unlock();
 
-        if ((result != OE_OK) || (ret != 0)){
-            printf(
-                "Host: verify_report_and_set_pubkey failed. %s",
-                oe_result_str(result));
-            if (ret == 0)
-                ret = 1;
+            free(pem_key);
+            pem_key = NULL;
+            free(remote_report);
+            remote_report = NULL;
         }
-        else
-            cnt++;
-
-        free(pem_key);
-        pem_key = NULL;
-        free(remote_report);
-        remote_report = NULL;
     }
 }
 
-void broadcast_report(uint8_t* pem_key, size_t pem_key_size, uint8_t* remote_report, size_t remote_report_size){
+int broadcast_report(uint8_t* pem_key, size_t pem_key_size, uint8_t* remote_report, size_t remote_report_size){
     struct report pkt;
     int status;
     pkt.index = my_index;
@@ -188,13 +263,15 @@ void broadcast_report(uint8_t* pem_key, size_t pem_key_size, uint8_t* remote_rep
     printf("Host: broadcasting report to oracle nodes...\n");
     for(int i = 0; i < oracle_num; i++){
         if (i == my_index) continue;
-        
-        status = sendto(sock, &pkt, sizeof(struct report), 0, (struct sockaddr*)&(peer_list[i]), sizeof(struct sockaddr_in));
-        if (status < 0){
+
+        printf("Host: send to node:%s:%d\n", inet_ntoa(peer_list[i].sin_addr), ntohs(peer_list[i].sin_port));
+        if (send(sock[i], (char *)&pkt, sizeof(struct report), 0) <= 0){
             fprintf(stderr, "send to %d-th oracle node fail.\n", i);
-            return;
+            return -1;
         }
     }
+    printf("Host: broadcasts successfully.\n");
+    return 1;
 }
 
 int main(int argc, const char* argv[])
@@ -210,9 +287,10 @@ int main(int argc, const char* argv[])
     size_t remote_report_size = 0;
 
     /* Additional vars  */
+    struct report my_report;
     char buf[20];
     int clntlen, flag = 1;
-    std::thread worker;
+    std::thread listener[10];
     //struct sigaction act;
 
     /* ./host enclave.signed -port:<port> */
@@ -223,16 +301,10 @@ int main(int argc, const char* argv[])
     }
 
     /* Read config file and init peer & socket info. */
-    if (init_peer_config() < 0)
-        return -1;
-
-    /* Start of initialization for socket. */
-    if ((sock = socket(PF_INET, SOCK_DGRAM, 0)) < 0){
-        fprintf(stderr, "Cannot open send stream socket.\n");
+    if (init_peer_config() < 0){
+        fprintf(stderr, "config file read fail.\n");
         return -1;
     }
-
-    //sigaction(SIGCHLD, &act, 0);
 
     /* Create a enclave. */
     printf("Host: Creating a enclaves\n");
@@ -262,33 +334,30 @@ int main(int argc, const char* argv[])
         goto exit;
     }
     printf("Host: enclave's public key: \n%s", pem_key);
+    printf("Host: key size = %zu, report size = %zu\n", pem_key_size, remote_report_size);
 
-
-    // bind
-    if (bind(sock, (struct sockaddr *)&my_addr, sizeof(my_addr)) < 0){
-        fprintf(stderr, "Cannot bind local address.\n");
+    if (connect_to_peer() < 0){
+        fprintf(stderr, "Host: connection fail.\n");
         goto exit;
     }
-   
-    /* Receive all reports from other oracle ndoes. */
-    worker = std::thread(recv_report, enclave);
-    
-    while(1){
-        printf("Press enter...(broadcast report & public key)\n");
-        fgets(buf, sizeof(buf), stdin);
-        if (buf[0] == '\n')
-            break;
+
+    for (int i = 0; i < oracle_num; i++){
+        if (i == my_index)  continue;
+        listener[i] = std::thread(recv_report, enclave, i);
     }
 
-    /* Broadcast to all oracle nodes. */
-    if (flag){
-        flag--;
-        broadcast_report(pem_key, pem_key_size, remote_report, remote_report_size);
+    if (broadcast_report(pem_key, pem_key_size, remote_report, remote_report_size) < 0){
+        fprintf(stderr, "Host: broadcast report fail.\n");
+        goto exit;
     }
 
-    /* Wait until all reports got verified. */
-    worker.join();
-    
+    for (int i = 0; i < oracle_num; i++){
+        if (i == my_index)  continue;
+        listener[i].join();
+    }
+
+    printf("Host: all oracle nodes verified.\n");
+
     // Set up tls connection between oracle nodes.
     /*printf("Host: calling setup_tls_server\n");
     ret = setup_tls_server(enclave, &ret, target_port);
@@ -297,67 +366,10 @@ int main(int argc, const char* argv[])
         printf("Host: setup_tls_server failed\n");
         goto exit;
     }*/
-    
-    /* ???????????????????????
-    free(pem_key);
-    pem_key = NULL;
-    free(remote_report);
-    remote_report = NULL;
-    */
 
-    /*
-    printf("Host: Requesting a remote report and the encryption key from "
-           "2nd enclave=====\n");
-    result = get_remote_report_with_pubkey(
-        enclave_b,
-        &ret,
-        &pem_key,
-        &pem_key_size,
-        &remote_report,
-        &remote_report_size);
-    if ((result != OE_OK) || (ret != 0))
-    {
-        printf(
-            "Host: verify_report_and_set_pubkey failed. %s",
-            oe_result_str(result));
-        if (ret == 0)
-            ret = 1;
-        goto exit;
-    }
-
-    printf("Host: 2nd enclave's public key: \n%s", pem_key);
-
-    printf("Host: Requesting first enclave to attest 2nd enclave's "
-           "remote report and the public key=====\n");
-    result = verify_report_and_set_pubkey(
-        enclave,
-        &ret,
-        pem_key,
-        pem_key_size,
-        remote_report,
-        remote_report_size);
-    if ((result != OE_OK) || (ret != 0))
-    {
-        printf(
-            "Host: verify_report_and_set_pubkey failed. %s",
-            oe_result_str(result));
-        if (ret == 0)
-            ret = 1;
-        goto exit;
-    }
-    free(pem_key);
-    pem_key = NULL;
-    free(remote_report);
-    remote_report = NULL;
-
-    printf("Host: Remote attestation Succeeded\n");
-
+ 
     // Free host memory allocated by the enclave.
-    free(encrypted_msg);
-    encrypted_msg = NULL;
-    ret = 0;
-    */
-
+ 
 exit:
     if (pem_key)
         free(pem_key);
