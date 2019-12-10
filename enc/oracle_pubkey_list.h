df0f58ab4445aa3766103abbba11979543b5fff8
qwerwon <hou@Oracle.sc3temum012u3livpuyxqmhubf.bx.internal.cloudapp.net>
2019-12-10 16:30:42 +0000

    bug

---
+++ enc/oracle_pubkey_list.h
@@ -0,0 +1,46 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#ifndef SAMPLES_REMOTE_ATTESTATION_PUBKEY_H
+#define SAMPLES_REMOTE_ATTESTATION_PUBKEY_H
+
+#define ENCLAVE_PUBKEY_SIZE 624
+
+static const char OTHER_ENCLAVE_PUBLIC_KEY[][624] = {
+    "-----BEGIN PUBLIC KEY-----\n"
+    "MIIBoDANBgkqhkiG9w0BAQEFAAOCAY0AMIIBiAKCAYEAv+pvC/+RfWciNVaEadDx\n"
+    "yly5hna8nvkaoRLeIPZ/6KZLl7xhhz0TfBsGfcloaUyYfw3xet0o6ttfAAD6dxvM\n"
+    "dPsovyng8B/WVk8kJMtM4Qu0FK2UJFNgu9jUABSVd8o4zfGqgdgE2fLBZ3cUrpfS\n"
+    "MYXQsq9Z2GCJuBMRouMM8biFIFej0EGWQM9NMa4ZOx7EN0fl14WJ2KgwDb1V22mN\n"
+    "q1Nu2wODCTh2K2S1rdMDNMMJG7bul2BzI7ReLf9mvfQWAFiT7XTBfqaa9XHqcbCE\n"
+    "I79E4yBJYwh5E+e4b2/m6AjmvtSz0EY6FbbRTSqhCFiDPqaTd0RrKgwTu65YeP2a\n"
+    "PfOU6h2SYDd23R4pddavuP7IfW4rnBCKUBa+3K5zdYc8eeLKEOwOTPMSXR2meSih\n"
+    "z+/DMVSWhMj15W8Qx/BB9WBkoTn3XvoVXxRCkY+NfmSfc7jM47MrTi1bMBRhPRDF\n"
+    "FjMCd1RjlinVcpbzHEP+Vz6ACimTewkVvUSpEkEj5IoFAgED\n"
+    "-----END PUBLIC KEY-----\n",
+
+    "-----BEGIN PUBLIC KEY-----\n"
+    "MIIBoDANBgkqhkiG9w0BAQEFAAOCAY0AMIIBiAKCAYEAxeDg8EGNHP3iQ2HDr/Tt\n"
+    "rOX8yy89yYOaJYyU2R/GRcnP+7lYsye0W6PYLSiT/r2hSdK3vKQ2gfQJpZIIyW75\n"
+    "8M4wtZCA+w/tvnMxHydMo0ds2G5YNVAzdxyC3wal5A9YbBsM6WRPevvTcbaYH9Gr\n"
+    "YLF8QSgxkIA86ssAmOM5eAes9GWy/ECilBspnKAlGHyG4a40I5IpMcQYmTiD7f8q\n"
+    "bVV6/MtLXaqz6Ucl/NvCtB56qxsbUyPEFeya2T8xFoT2qNh7jyOqAxnwyiwCK/RB\n"
+    "zoC9J58vFYZVjIggQkrc4vjZEbuwGvuy2vVTmroZTi99z9p8ZlEqxw/KetASQmzV\n"
+    "GoQvcP/AUYXEr6u3yPnH7SXAhZuCiWj4fYy5Q95TrEUPFaZNJ09E+8YlghK2+TuZ\n"
+    "LIZFVN8JCY1hY18n4nrmi2pJZtMStNd9rhF4c2OIWQHH1gfqSrjs2dIedr/V7LOD\n"
+    "Zx2ffqPF2sXHdt5UPECqbYSzKI/FSnFYFpaesJBU/9ITAgED\n"
+    "-----END PUBLIC KEY-----\n",
+
+    "-----BEGIN PUBLIC KEY-----\n"
+    "MIIBoDANBgkqhkiG9w0BAQEFAAOCAY0AMIIBiAKCAYEA0XlWequyqllLxyk6czRY\n"
+    "0eQnWgrx2z/FqnfoLMM7MN2luH67rlvxRDhA6Obx12VPXGjxf2kFhLV4BirEs1/h\n"
+    "/wXGDQZwyRz2Dr8UnMu3B5uts1ByVs+gPN7c7MuZQx3W0rh4Gpso3LnoGjp/zez8\n"
+    "vZ0RiGpE0ByGSR7qXcPwliDrAOgkTVrr8MCPBwWCG1Otk8+36aIBmQrxAR+z68rx\n"
+    "ykCNHoBxhWFkWtOSfrBCsZBqcwjZS2xpAje0KrcWqIl837OCDq6D2COJthD7B/3n\n"
+    "Icg07QFi5ePGwzhIJbja2G0HnSXb0OQETELBWDIIuNmp5rpNeMUKrnQVE7uEEaiL\n"
+    "xm5Mjvgv6EVrA+RFrMYGvIp3IYrf/FHsBywCYBMmsddCHXwUFfAeEnWJEfeMvvAL\n"
+    "HexP994MfzvEn0ayl4NG8RLzPWkNuMaqlirIvnWxByKiWxieO0vmmbWyXIwTrmCL\n"
+    "OI5BIMaEkgpavyozPrmSuIBtr69kdrKJ7/HkZ7HmmE3TAgED\n"
+    "-----END PUBLIC KEY-----\n"};
+
+#endif /* SAMPLES_REMOTE_ATTESTATION_PUBKEY_H */
--- host/exe.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-./oracle_host ../enc/oracle_enc.signed -port:12341
